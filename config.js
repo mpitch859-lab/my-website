26bebd4dea00d2e3486dc405c1ff7570322d6cef
mpitch859-lab <mpitch859@gmail.com>
2025-12-10 04:48:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // js/config.js
-export const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxRh1UxznKhZ2nQDY3x7OyAZikATie58dDbJzVglrTDifzqSKlg2swWYwsVfACsntYq/exec";
+export const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwMl-LT8Sy_10Myk4PgLC4VLyOKO4cdRQze4t746xA4TYYWwETOYquMXB0VETFuFU0r/exec";
