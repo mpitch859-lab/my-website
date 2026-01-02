a1c7162280359dd15d57151471dad4ad29a61bbc
mpitch859-lab <mpitch859@gmail.com>
2026-01-02 17:25:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // js/config.js
-export const API_URL = "https://script.google.com/macros/s/AKfycbx8bXLjqJHyD5iN0qjqh9UvA9NLGRJ4ZJ2xiiJw0PnXPXzQsThomgD1BOddoa44jR_x/exec";
+export const API_URL = "https://script.google.com/macros/s/AKfycbwIZMf7nYyZB1Qe21tX_qwEYEUHSJ1LInP_xhgne0o34Q264uvEWY1NITUyaeGtQb87/exec";
