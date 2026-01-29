da5b539aac7fabda14c421bd83b195d2fa37c5da
mpitch859-lab <mpitch859@gmail.com>
2026-01-29 22:00:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // js/config.js
-export const GAS_URL ="https://script.google.com/macros/s/AKfycbwu53Dlx3XHbQiIuQasgD87bTXT1nBnWpooGMKw-P-BRNsYwesRUJ4CXay5rcvDdAGJ/exec";
+export const GAS_URL ="https://script.google.com/macros/s/AKfycbz9IAtQJtDRnvn7KpISX4aJRlhXZKTiN4DVM_eFbKBpL0s5Yho_QlRW9l7rwRpfO4r7/exec";
