5db2a05bf35e32e248b9c8553a7ba7c9cd90599b
mpitch859-lab <mpitch859@gmail.com>
2026-01-30 06:31:21 +0700

    Add files via upload

--- api-client.js
+++ api-client.js
@@ -1,26 +1,16 @@
 import { GAS_URL } from "./config.js";
-console.log("กำลังส่งข้อมูลไปที่:", GAS_URL);
 
-// สำคัญมาก: ต้องมีคำว่า export เพื่อให้ไฟล์อื่นดึงไปใช้ได้
 export async function callApi(action, data = {}) {
     try {
         const token = sessionStorage.getItem("session_token");
-        const bodyData = {
-            action,
-            token,
-            ...data
-        };
-
+        const bodyData = { action, token, ...data };
         const response = await fetch(GAS_URL, {
             method: "POST",
             body: JSON.stringify(bodyData)
         });
-
         const result = await response.json();
         
-        // ตรวจสอบ Session หมดอายุ
         if (!result.success && result.error && result.error.includes("Session expired")) {
-            // ถ้าไม่ใช่หน้า login ให้แจ้งเตือนและส่งกลับไป login
             if (!window.location.pathname.includes("login.html")) {
                 alert("เซสชันหมดอายุ กรุณาเข้าสู่ระบบใหม่");
                 sessionStorage.clear();
@@ -28,7 +18,6 @@ export async function callApi(action, data = {}) {
             }
             return;
         }
-
         if (!result.success) throw new Error(result.error);
         return result;
     } catch (error) {
--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // js/config.js
-export const GAS_URL ="https://script.google.com/macros/s/AKfycbzuNmf42pz-dQ9_gl__M0RwO9zPV8tVzX2YNBJarXGibGFfYbdJJRqeE7zw1_gWmRu6/exec";
+export const GAS_URL ="https://script.google.com/macros/s/AKfycbzxT_wgxuO-Ga3GJdudAgK1PcwC0N2ItuIaQ3CMxw0s2cjds2SNf7Ewe2f95wtS94BA/exec";
