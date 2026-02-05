1988764f90a3d219bc650858d401c9588460f81d
mpitch859-lab <mpitch859@gmail.com>
2026-02-05 11:11:54 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // js/config.js
-export const GAS_URL ="https://script.google.com/macros/s/AKfycbyXDHAOzzKsoYWvL5uAz5XfM3LLDloZvDIui73e5AEOke8c1qnMIOuweUvdqXmLwrur/exec";
+export const GAS_URL ="https://script.google.com/macros/s/AKfycbylPCdCKMdRgBJS1-hIUH3Z3sbzhsJTOw6-SUmSd59yIxHsN68yK8KADCz6mnYBqevg/exec";
