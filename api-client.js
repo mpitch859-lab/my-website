6aa9dcd8939d9721bea5ebfa3502aaa3a8f53a93
mpitch859-lab <mpitch859@gmail.com>
2026-02-05 18:09:33 +0700

    Add files via upload

--- api-client.js
+++ api-client.js
@@ -2,18 +2,13 @@
 import { GAS_URL } from "./config.js";
 
 export async function callApi(action, data = {}) {
-  try {
-    const token = sessionStorage.getItem("session_token");
-    const body = { action, token, ...data };
+  const res = await fetch(GAS_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ action, ...data })
+  });
 
-    const res = await fetch(GAS_URL, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(body)
-    });
-
-    return await res.json();
-  } catch (err) {
-    return { success: false, error: "เชื่อมต่อเซิร์ฟเวอร์ไม่ได้" };
-  }
+  return await res.json();
 }
--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // js/config.js
-export const GAS_URL ="https://script.google.com/macros/s/AKfycbyXImCclM3HTOY_iWd9w--_w1sQYYZ8HcYJLT1kyj362KBgxPq8xYSSqJX5JYouN5k6/exec";
+export const GAS_URL ="https://script.google.com/macros/s/AKfycby0AXgj5ceYa8dOE8sqKIDdjw_DPcOcVDxqoIF2RLuxgOnPJouvF65tQGx5y6NLNeXd/exec";
